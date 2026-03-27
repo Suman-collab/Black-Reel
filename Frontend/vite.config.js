f75ad9ccab294e87fd904322d84fb60b473dee33
Suman <s61354922@gmail.com>
2026-03-27 18:13:15 +0530

    add

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://black-reel-q6b7.vercel.app',
         changeOrigin: true,
       },
     },
